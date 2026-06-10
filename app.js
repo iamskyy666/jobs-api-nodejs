e9166429c5cfddc736e25b2ad535a9c19cdb1dc1
Soumadip <39thdecember@gmail.com>
2026-06-10 23:17:15 +0530

    Connect to MongoDB 🛜

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ import notFoundMiddleware from "./middleware/not-found.middleware.js";
 import errorHandlerMiddleware from "./middleware/error-handler.middleware.js";
 import authRouter from "./routes/auth.router.js";
 import jobsRouter from "./routes/jobs.router.js";
+import connectDB from "./db/connectDB.js";
 
 const app = express();
-
-// mongo-DB connection
 
 app.use(express.json());
 
@@ -25,6 +24,9 @@ app.use("/api/v1/jobs", jobsRouter);
 // middlewares
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
+// mongo-DB connection
+connectDB(process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 3000;
 
