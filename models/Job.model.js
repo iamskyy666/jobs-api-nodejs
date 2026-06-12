58a852a020a3cb0705b22d9b0becaf69f9e989d7
Soumadip <39thdecember@gmail.com>
2026-06-12 21:30:56 +0530

    Create Job Model 💼

---
+++ models/Job.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "🔴 Please provide company name!"],
+      maxLength: 40,
+    },
+    position: {
+      type: String,
+      required: [true, "🔴 Please provide position!"],
+      maxLength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "🔴 Please provide user!"],
+    },
+  },
+  { timestamps: true },
+);
+
+const Job = mongoose.model("Job", JobSchema);
+export default Job;
