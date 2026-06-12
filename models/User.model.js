cd3b0f12cf2ffeee830ccc8afca6fe1a388bcb67
Soumadip <39thdecember@gmail.com>
2026-06-12 16:48:40 +0530

    Refactor - Mongoose Middleware 🔧

--- models/User.model.js
+++ models/User.model.js
@@ -31,6 +31,9 @@ const UserSchema = new Schema({
 //! Mongoose Middleware - Hash Password 🟢
 // ⚠️ Don't use arrow f(x) - Scoping issues with 'this' kw.
 UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
