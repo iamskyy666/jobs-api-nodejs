024ea8174e007d5f5f7d1ee60a24bbd913e7e30d
Soumadip <39thdecember@gmail.com>
2026-06-12 14:42:41 +0530

    Hash Password 🎭

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/User.model.js";
 import { BadRequestError } from "../errors/index.js";
+import bcrypt from "bcryptjs";
 
 const register = async (req, res) => {
-  
-  const user = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
