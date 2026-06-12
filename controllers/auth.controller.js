527bea129e6be14e9a0b97ec49b6d26b04507b19
Soumadip <39thdecember@gmail.com>
2026-06-12 16:41:06 +0530

    Generate JWT - Instance Method 🔑

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,8 +2,18 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/User.model.js";
 
 const register = async (req, res) => {
+  // Create user
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  // Generate token - instance method on User model
+  const token = user.generateJWT();
+
+  res.status(StatusCodes.CREATED).json({
+    message: "✅ User registered successfully!",
+    user: { name: user.name }, // getName()
+    token: token,
+    error: false,
+  }); // acc. to our needs (maybe the frontend needs the name and so on..)
 };
 
 const login = async (req, res) => {
--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import validator from "validator";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 const UserSchema = new Schema({
@@ -27,12 +28,27 @@ const UserSchema = new Schema({
   },
 });
 
-// Mongoose middleware - hash password
+//! Mongoose Middleware - Hash Password 🟢
 // ⚠️ Don't use arrow f(x) - Scoping issues with 'this' kw.
 UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Instance Method 🟢
+// example - get user name
+// UserSchema.methods.getName = function () {
+//   return this.name;
+// };
+
+//! Generate Token - Instance Method 🟢
+UserSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME },
+  ); // userId, jobId - better naming convention, less confusing than user._id or job._id
+};
 
 const User = mongoose.model("User", UserSchema);
 export default User;
